54d75e6d310745062ada9d998f7968e9d7732dc1
ansehyeon090109 <ansehyeon09010@gmail.com>
2025-09-16 02:18:08 +0900

    버튼클릭

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let input = document.getElementById('input');
-let output = document.getElementById('output')
+let output = document.getElementById('output');
+let button = document.getElementById('button');
+
+button.addEventListener('click',()=>{
+    alert('버튼클릭');
+})
+
 input.addEventListener('input', () => {
     output.textContent = input.value;
 })
